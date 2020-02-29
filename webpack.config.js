5ba7d032342e22991fcf53e83784fec98851e936
Alan Anderson <anderson1941@sbcglobal.net>
2020-02-29 10:30:36 -0600

    2/29/2020 10:30 AM

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = ({ production } = {}, {extractCss, analyze, tests, hmr, port, h
   mode: production ? 'production' : 'development',
   output: {
     path: outDir,
-    publicPath: baseUrl,
+    publicPath: production ? '/au-input-currency/' : baseUrl,
     filename: production ? '[name].[chunkhash].bundle.js' : '[name].[hash].bundle.js',
     sourceMapFilename: production ? '[name].[chunkhash].bundle.map' : '[name].[hash].bundle.map',
     chunkFilename: production ? '[name].[chunkhash].chunk.js' : '[name].[hash].chunk.js'
